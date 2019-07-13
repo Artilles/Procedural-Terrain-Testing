4ffe80b6b0df8ad12a987f52f8821dcefd03e03a
Zac Bell <fretold.zb@gmail.com>
2019-07-13 13:04:57 -0700

    Mesh rendering now has a height multiplier and and height curve (using AnimationCurve)

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -23,6 +23,9 @@ public class MapGenerator : MonoBehaviour
     public float lacunarity = 2f;
     public Vector2 offset;
 
+    public float meshHeightMultiplier = 1f;
+    public AnimationCurve meshHeightCurve;
+
     public TerrainType[] regions;
 
     public void GenerateMap()
@@ -49,7 +52,7 @@ public class MapGenerator : MonoBehaviour
         else if (drawMode == DrawMode.ColorMap)
             display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
         else if (drawMode == DrawMode.Mesh)
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve), TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
 
     }
 
