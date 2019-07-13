ba88aa5611080c5d406917acc05db5a01f91ab2a
Zac Bell <fretold.zb@gmail.com>
2019-07-12 20:49:17 -0700

    Initial project + noise map generation

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapWidth = 10;
+    public int mapHeight = 10;
+    public float noiseScale = 0.3f;
+    public bool autoUpdate = false;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = NoiseGenerator.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class NoiseGenerator
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        if (scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float[,] noiseMap = new float[mapWidth, mapHeight];
+
+        for(int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y /scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
